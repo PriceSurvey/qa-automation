5000776b8451256f3f94c19f3bcd4f622d408339
Arthur Pires <arthurclp@icloud.com>
2023-08-24 18:28:18 -0300

    Update slack-notifications.ts

--- slack-notifications.ts
+++ slack-notifications.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const sendMessageToSlackChannel = async (channel: string, msg: string) => {
+  return true;
   return axios.post("https://n8n.pricesurvey.io/webhook/qa-notification", {
     channelName: channel,
     msg,
-  });
+  }).catch((error)=>console.log('error: ', error);
 };
